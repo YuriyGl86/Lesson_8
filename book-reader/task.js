1479a57aaeb7a10be8cc5d2eb68159e9301e9591
YuriyGl86 <yuriygl86@gmail.com>
2023-04-29 21:55:39 +0300

    final

--- book-reader/task.js
+++ book-reader/task.js
@@ -2,37 +2,53 @@ const book = document.querySelector('#book')
 const dataSetParams = {
     size: {small: 'book_fs-small',
             big: 'book_fs-big',
-        }
+    },
+    textColor: {black: 'book_color-black',
+                gray: 'book_color-gray',
+                whitesmoke: 'book_color-whitesmoke',
+    },
+    bgColor: {black: 'book_bg-black',
+              gray: 'book_bg-gray',
+              white:  'book_bg-white',
+    },
     
 }
 
 class Button {
 
-    constructor(param, classSelector){
+    constructor(param, classSelector, classActive){
         this.param = param
+        this.classActive = classActive
         this.buttonList = Array.from(document.querySelectorAll(classSelector))
-        console.log(this.buttonList)
         this.buttonList.forEach(elem => elem.addEventListener('click', this.buttonClickHandler.bind(this)))
     }
 
     removeAllClasses(){
-        book.classList.remove('book_fs-small')
-        book.classList.remove('book_fs-big')
+        const classes = Object.values(dataSetParams[this.param])
+        classes.forEach(elem => book.classList.remove(elem))
+
     }
 
-    buttonClickHandler(event){
-        
+    buttonClickHandler(event){        
         event.preventDefault()
         const newValue = event.target.dataset[this.param]
-        console.log(newValue)
 
         this.removeAllClasses()
 
         if(newValue){book.classList.add(dataSetParams[this.param][newValue])}
+        this.setActive(event.target)
 
+    }
+
+    setActive(elem){
+        const currentActive = this.buttonList.find(elem => elem.classList.contains(this.classActive))
+        currentActive.classList.remove(this.classActive)
+        elem.classList.add(this.classActive)
     }
 
     
 }
 
-new Button('size', '.font-size')
+new Button('size', '.font-size', 'font-size_active')
+new Button('textColor', '.book__control_color>.color', 'color_active')
+new Button('bgColor', '.book__control_background>.color', 'color_active')
